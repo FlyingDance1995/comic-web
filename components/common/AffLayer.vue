395b58d04fd3ece1db8fef7f9377c7f88d4c49f8
Dang Tuan Minh <dtminh2808@gmail.com>
2024-07-11 08:42:47 +0700

    update ads

--- components/common/AffLayer.vue
+++ components/common/AffLayer.vue
@@ -23,5 +23,15 @@ onMounted(() => {
 </template>
 
 <style scoped>
-
+.transparent-layer {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0);
+    z-index: 9999999999;
+    justify-content: center;
+    align-items: center;
+}
 </style>
