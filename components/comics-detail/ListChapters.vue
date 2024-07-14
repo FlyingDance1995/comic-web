32b8c7cfac2cbf51281f299419fc2ae97d396843
hoannq <hoannq95@gmail.com>
2024-07-14 09:08:14 +0700

    Update type

--- components/comics-detail/ListChapters.vue
+++ components/comics-detail/ListChapters.vue
@@ -1,7 +1,7 @@
 <script setup>
 
-import {timeAgo} from "~/utils/formatTime.js";
-
+import { timeAgo } from "~/utils/formatTime.js";
+import { formattedNameChaper } from "~/utils/formatName.js";
 const route = useRoute();
 
 const slug = route?.params?.slug;
@@ -15,7 +15,7 @@ const data = ref(null);
 
 const getData = async () => {
     try {
-        const {data: story} = await useAPI(`/story/${slug}/chapter`, {
+        const { data: story } = await useAPI(`/story/${slug}/chapter`, {
             query: query
         });
         data.value = story?.value;
@@ -28,15 +28,14 @@ if (slug) getData();
 </script>
 <template>
     <div class="list-chapters">
-        <div v-for="item in data?.results"
-            :key="item?.id"
-            class="item d-flex justify-content-between">
+        <div v-for="item in data?.results" :key="item?.id" class="item d-flex justify-content-between">
             <div class="episode-title">
                 <a :href="`/${slug}/chuong-${item?.chapter_number}`">
-                    Chương {{item?.chapter_number}}
-                </a></div>
+                    {{ formattedNameChaper(item?.type) }} {{ item?.chapter_number }}: {{ item?.name }}
+                </a>
+            </div>
             <div class="episode-date">
-                <span>{{timeAgo(item?.creation_time)}}</span>
+                <span>{{ timeAgo(item?.creation_time) }}</span>
             </div>
         </div>
     </div>
