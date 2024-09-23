808f66bdec5ba4801b3937a4c6e5debcbcbd24ce
hoannq <hoannq@1link.vn>
2024-09-23 17:28:29 +0700

    1800

--- components/common/AffLayerModal.vue
+++ components/common/AffLayerModal.vue
@@ -1,6 +1,6 @@
 <script setup>
-import {useConfigStore} from "~/store/config.js";
-import {useUserStore} from "~/store/user.js";
+import { useConfigStore } from "~/store/config.js";
+import { useUserStore } from "~/store/user.js";
 
 const userStore = useUserStore();
 const configStore = useConfigStore();
@@ -18,14 +18,14 @@ const handleClickOutside = () => {
     configStore.setAffModal(false);
     setTimeout(() => {
         configStore.setAffModal(true);
-    }, 10000);
+    }, 90000);
 };
 
 const handleAffLayerClick = () => {
     configStore.setAffModal(false);
     setTimeout(() => {
         configStore.setAffModal(true);
-    }, 600000);
+    }, 1800000);
 };
 
 const checkSessionStorage = () => {
@@ -39,7 +39,7 @@ watch([checkVIP, aff], () => {
     if (checkVIP.value || !aff.value) {
         configStore.setAffModal(false);
     }
-}, {immediate: true})
+}, { immediate: true })
 
 watch(affModal, () => {
     if (affModal.value) {
@@ -55,40 +55,35 @@ watch(affModal, () => {
             isBlock.value = false;
         }, 150);
     }
-}, {immediate: true});
+}, { immediate: true });
 </script>
 
 <template>
-    <div class="modal fade"
-         :class="isOpen ? 'show' : ''"
-         id="reportModal" tabindex="-1" role="dialog"
-         :aria-modal="affModal" :aria-hidden="!affModal"
-         :style="{display: isBlock ? 'block' : 'none'}">
+    <div class="modal fade" :class="isOpen ? 'show' : ''" id="reportModal" tabindex="-1" role="dialog"
+        :aria-modal="affModal" :aria-hidden="!affModal" :style="{ display: isBlock ? 'block' : 'none' }">
         <div class="modal-dialog" role="document" style="margin-top: 30px">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="btn-close" @click="handleClickOutside"
-                            data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" @click="handleClickOutside" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
                 </div>
 
                 <div class="modal-body text-center" style="font-size: 15px">
                     <p>
                         Để có kinh phí và năng lượng tiếp tục dịch và ra mắt các bộ truyện hay.
                         <br>
-                        <b>Phê Truyện</b> rất mong mọi người có thể <b>Click</b> vào link hoặc hình ảnh bên dưới để ủng hộ <b>Phê Truyện</b>.
+                        <b>Phê Truyện</b> rất mong mọi người có thể <b>Click</b> vào link hoặc hình ảnh bên dưới để ủng
+                        hộ <b>Phê Truyện</b>.
                         <br>
                     </p>
                     <p>
-                        <a :href="aff?.link" target="_blank" style="display:block;"
-                           @click="handleAffLayerClick">
-                            <img :src="aff?.banner" alt=""
-                                 style="display: block; width: 320px; margin: 15px auto 10px">
+                        <a :href="aff?.link" target="_blank" style="display:block;" @click="handleAffLayerClick">
+                            <img :src="aff?.banner" alt="" style="display: block; width: 320px; margin: 15px auto 10px">
                         </a>
                     </p>
                     <p>
-                        <a :href="aff?.link" target="_blank" class="text-primary"
-                           @click="handleAffLayerClick">
-                            <b>{{aff?.link}}</b>
+                        <a :href="aff?.link" target="_blank" class="text-primary" @click="handleAffLayerClick">
+                            <b>{{ aff?.link }}</b>
                         </a>
                     </p>
                     <p>
